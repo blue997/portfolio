430aed23dbec9bb56acff078303542a02a951386
blue997 <blue.wave.surf.ocean@gmail.com>
2025-02-21 13:22:35 +0900

    second modify

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+const menuBtn=document.querySelector('.fa-bars')
+const showMenu=document.querySelector('.gnb')
+
+menuBtn.addEventListener('click',()=>{
+  showMenu.classList.toggle('active')
+})
+
+
 
 const toTop=document.querySelector('#totop')
+/* window.addEventListener('scroll',_.throttle(()=>{
+  if(window.scrollY>500) {
+    gsap.to(toTop,.7,{
+      x:0, opacity:1
+    })
+  } else {
+    gsap.to(toTop,.7,{
+      x:100
+    })
+  }
+},300)) */
 
 toTop.addEventListener('click',()=>{
   gsap.to(window,.7,{
@@ -18,16 +29,20 @@ toTop.addEventListener('click',()=>{
 
 const spyEls=document.querySelectorAll(".scroll-spy")
 spyEls.forEach(function(spyEl){
-  // Screen() 메서드를 통해 여러 객체들의 변화에 대한 검사
-  // addTo() 메서드를 사용하기 위한 옵션 정의
   new ScrollMagic
   .Scene({
-    // 보여질 여부를 검사하는 요소 지정
     triggerElement:spyEl,
-    // 화면의 높이를 0에서 1이라 보고 0.8 위치에 적용
-    // 기능이 걸려 있는 부분(실행위치)
     triggerHook:.8
   })
   .setClassToggle(spyEl,'show')
   .addTo(new ScrollMagic.Controller());
 })
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
